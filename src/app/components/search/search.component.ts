c2cdc7d54d11c15b2a6ec2e50c7df6f57088fdfe
Miquel Puig <mpuig22@gmail.com>
2019-03-04 21:04:06 +0100

    primera optimización

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -14,8 +14,7 @@ export class SearchComponent  {
 
   buscar(termino: string) {
     this.spotify.getArtista(termino).subscribe((data: any) => {
-      console.log(data.artists.items);
-      this.artistas = data.artists.items;
+      this.artistas = data;
     });
   }
 
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
+import { map } from 'rxjs/operators';
+
+
 @Injectable({
   providedIn: 'root'
 })
@@ -14,7 +17,9 @@ export class SpotifyService {
     'Authorization': 'Bearer BQAbf-ZsuDrZaeFXUrIxcJBLQSJzhbQ-S5fZK4a3tJKhEmD7RRmRGo8vOsf5Jy9xK0jZAnD4ODsXWd0-pnc'
     });
 
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', {headers});
+    return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', {headers})
+    .pipe( map( data => data['albums'].items
+    ));
 
   }
 
@@ -24,7 +29,9 @@ export class SpotifyService {
     'Authorization': 'Bearer BQAbf-ZsuDrZaeFXUrIxcJBLQSJzhbQ-S5fZK4a3tJKhEmD7RRmRGo8vOsf5Jy9xK0jZAnD4ODsXWd0-pnc'
     });
 
-    return this.http.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist&offset=0&limit=20`, {headers});
+    return this.http.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist&offset=0&limit=20`, {headers})
+    .pipe( map ( data => data['artists'].items
+    ));
 
   }
 
